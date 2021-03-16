a09cb296fc4508cd06f849328e2c18a3ac86985b
Tanjil Pranto <53004677+tanjil072@users.noreply.github.com>
2021-03-16 23:44:53 +0600

    search done

--- options.sql
+++ options.sql
@@ -9,8 +9,16 @@ DECLARE
      dbms_output.put_line('1. Insert');
      dbms_output.put_line('2. Delete');
 	 dbms_output.put_line('3. Search');
+	 
+	 dbms_output.put_line(CHR(10));
+	 dbms_output.put_line('Search Options:');
+	 dbms_output.put_line('Type 1 for age>2');
+	 dbms_output.put_line('Type 2 for Language=Bangla');
+	 dbms_output.put_line(CHR(10));
+		
+	 
    END;
-  /	
+ /	
   
   
   
@@ -27,12 +35,9 @@ DECLARE
 	language varchar2(50);
 	
 	delete_id integer;
+	searchOption number;
 	
 	noInput EXCEPTION;
-	
-	
-	
-	
 	
  BEGIN
      
@@ -64,9 +69,24 @@ DECLARE
 		
 		mypack.Delete_Entry(delete_id);
 		
-	ELSIF A = 3 THEN 
-		--dbms_output.put_line('Search');
-		mypack.Search_Entry;
+	ELSIF A = 3 THEN
+		searchOption:=&choose_How_You_Want_To_Search;
+	
+		IF searchOption=1 THEN
+			
+				
+			DBMS_OUTPUT.PUT_LINE(CHR(10) ||'Search By Age(Greater than 2):'||CHR(10));
+			FOR R IN (select name,age,gender,religion from data1 where age>2 union all select name,age,gender,religion from data1@site1 where age>5 order by age) LOOP
+	
+				DBMS_OUTPUT.PUT_LINE('Name:'||R.name||' ||' || ' Age:'||R.age ||' || ' || ' Gender:' ||R.gender ||' || ' || ' Religion:' ||R.religion); 
+		
+			END LOOP;
+				
+		
+		END IF;
+		
+		
+		--mypack.Search_Entry;
 		
 	END IF;
 	
